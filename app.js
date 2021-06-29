570d36daa7f12a6625e758f46d056accd18dbf68
lmaaljohare <68349552+lmaaljohare@users.noreply.github.com>
2021-06-29 23:32:31 +0300

    server files

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const cors=require('cors')
+const bodyParser = require('body-parser')
+
+var corsOptions =
+{
+    origin: 'http://localhost:4200',
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+app.use(bodyParser.json())
+app.listen(8000, () => {
+    console.log('Server started!')})
+
+
+    // test_endpoint
+app.get('/', (req,res)=>{
+ res.status(200).json("done..")})
+
